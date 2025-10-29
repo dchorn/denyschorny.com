778e4a883494a69852eec7d8e96b4d324db855c7
Denys Chorny <denis.man.07@gmail.com>
2025-10-29 20:10:58 +0100

    Add TODO REST API project and update personal info

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,26 +2,30 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 import PresentationCard from "../components/ui/presentation-card";
 
 export default function Home() {
-    return (
-        <Box>
-            <PresentationCard />
+  return (
+    <Box>
+      <PresentationCard />
 
-            <Box marginTop={"20px"}>
-                <Heading size={"xl"}>Who am I?</Heading>
-                <Text>
-                    My name is Denys, I am a 22yo programmer from Spain who enjoys crafting 📖 libraries, 💻 APIs, 🎮 games, 🛍️ web stores and 🧬 bioinformatics applications.
-                </Text>
+      <Box marginTop={"20px"}>
+        <Heading size={"xl"}>Who am I?</Heading>
+        <Text>
+          My name is Denys, I am a 24yo programmer from Spain who enjoys
+          crafting 📖 libraries, 💻 APIs, 🎮 games, 🛍️ web stores and 🧬
+          bioinformatics applications.
+        </Text>
 
-                <Text marginTop={"20px"}>
-                    I am incredibly passionate about programming and 🌱 sustainability. Though I am a fullstack developer, my preference lies with backend development.
-                </Text>
+        <Text marginTop={"20px"}>
+          I am incredibly passionate about programming and 🌱 sustainability.
+          Though I am a fullstack developer, my preference lies with backend
+          development.
+        </Text>
 
-                <Text marginTop={"20px"}>
-                    My go-to programming languages are 💛 JavaScript and 🐍 Python. Outside of coding, I have a liking for 🍵 tea, 🏋️‍♂️ calisthenics, 📺 anime, 🎵 music, and 📚 books.
-                </Text>
-
-            </Box>
-
-        </Box>
-    )
+        <Text marginTop={"20px"}>
+          My go-to programming languages are 💛 JavaScript and 🐍 Python.
+          Outside of coding, I have a liking for 🍵 tea, 🏋️‍♂️ calisthenics, 📺
+          anime, 🎵 music, and 📚 books.
+        </Text>
+      </Box>
+    </Box>
+  );
 }
